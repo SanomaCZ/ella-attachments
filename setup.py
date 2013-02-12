fe3794f1f6ac1ad6e03e56000c03ebc06ae82691
Vitek Pliska <whit@jizak.cz>
2013-02-12 16:11:09 +0100

    Standard python packaging

--- setup.py
+++ setup.py
@@ -1,17 +1,44 @@
 from setuptools import setup, find_packages
 
-VERSION = (0, 0, 1)
+VERSION = (0, 1, 0)
 __version__ = VERSION
 __versionstr__ = '.'.join(map(str, VERSION))
 
-setup(
-    name = 'ella_attachments',
-    version = __versionstr__,
+install_requires = [
+    'ella>=2,<4',
+    'South>=0.7',
+]
 
-    packages = find_packages(
-        where = '.',
-        exclude = ('tests',)
+setup(
+    name='ella_attachments',
+    version=__versionstr__,
+    description='Add attachments to Ella Publishable',
+    author='SMP Online Development Team',
+    author_email='online-dev@sanomamedia.cz',
+    maintainer='Vitek Pliska',
+    maintainer_email='whit@smdev.cz',
+    license='BSD',
+    url='https://github.com/SanomaCZ/ella-attachments',
+
+    packages=find_packages(
+        where='.',
+        exclude=('test_ella_attachments',)
     ),
 
-    include_package_data = True,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.5",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+
+    include_package_data=True,
+    install_requires=install_requires,
 )
