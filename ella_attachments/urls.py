6372a0c6fdf03514cb31e4ebbee369be202412d4
michal dub <michal.dub@sanomamedia.cz>
2016-09-01 16:05:04 +0200

    changed urlpatterns to new style

--- ella_attachments/urls.py
+++ ella_attachments/urls.py
@@ -1,13 +1,10 @@
 from django.utils.translation import ugettext as _
+from django.conf.urls import url
 
-try:
-    from django.conf.urls import patterns, url
-except ImportError:  # Django < 1.4
-    from django.conf.urls.defaults import patterns, url
 
 from ella_attachments.views import download_attachment
 from ella_attachments.utils import slugify
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^%s/(?P<slug>.*)' % slugify(_('download')), download_attachment, name='ella_attachments-download'),
-)
+]
